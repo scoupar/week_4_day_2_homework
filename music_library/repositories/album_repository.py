7c25ad7ff79b0834519a0a1834e0148ef8d9fd8f
Scott Coupar <scottcoupar@Scotts-MacBook-Pro.local>
2021-01-26 18:31:07 +0000

    first commit

---
+++ music_library/repositories/album_repository.py
@@ -0,0 +1,32 @@
+from db.run_sql import run_sql
+
+from models.album import Album
+from models.artist import Artist
+
+import repositories.artist_repository as artist_repository
+
+
+def save(album):
+    pass
+
+
+def delete_all():
+    pass
+
+
+def select(id):
+    pass
+
+
+def select_all():
+    pass
+
+
+# Extensions
+
+def delete(id):
+    pass
+
+
+def update(album):
+    pass
---
+++ music_library/repositories/artist_repository.py
@@ -0,0 +1,35 @@
+from db.run_sql import run_sql
+
+from models.artist import Artist
+from models.album import Album
+
+
+def save(artist):
+    pass
+
+
+def delete_all():
+    pass
+
+
+def select(id):
+    pass
+
+
+def select_all():
+    pass
+
+
+# Extensions
+
+
+def albums(artist):
+    pass
+
+
+def delete(id):
+    pass
+
+
+def update(artist):
+    pass

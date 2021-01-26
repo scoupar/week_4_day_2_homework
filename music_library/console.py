a8951ed063df9899c99e80710f9402db05987f2b
Scott Coupar <scottcoupar@Scotts-MacBook-Pro.local>
2021-01-26 20:48:52 +0000

    finished artist repository

--- music_library/console.py
+++ music_library/console.py
@@ -5,5 +5,15 @@ from models.album import Album
 import repositories.artist_repository as artist_repository
 import repositories.album_repository as album_repository
 
+artist_repository.delete_all()
+
+artist1 = Artist("Jimmy Eat World")
+artist_repository.save(artist1)
+
+artist2 = Artist("Pinegrove")
+artist_repository.save(artist2)
+
+artist_repository.select(17)
+
 
 pdb.set_trace()
--- music_library/repositories/artist_repository.py
+++ music_library/repositories/artist_repository.py
@@ -1,23 +1,40 @@
 from db.run_sql import run_sql
-
 from models.artist import Artist
 from models.album import Album
 
 
 def save(artist):
-    pass
+    sql = "INSERT INTO artists (name) VALUES (%s) RETURNING *"
+    values = [artist.name]
+    results = run_sql(sql, values)
+    id = results[0]['id']
+    artist.id =id
+    return artist
 
 
 def delete_all():
-    pass
+    sql ="DELETE FROM artists"
+    run_sql(sql)
 
 
 def select(id):
-    pass
+    artist = None
+    sql ="SELECT * FROM artists WHERE id = %s"
+    values = [id]
+    result = run_sql(sql, values)[0]
+    if result is not None:
+        artist = Artist(result['name'], result['id'])
+        return artist
 
 
 def select_all():
-    pass
+    artists = []
+    sql ="SELECT * FROM artists"
+    results =run_sql(sql)
+    for row in results:
+        artist=Artist(row['name'], row['id'])
+        artists.append(artist)
+    return artists
 
 
 # Extensions
